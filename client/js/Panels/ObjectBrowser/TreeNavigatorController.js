3bc43c0c3d0edf27901b6922354d9fa9889a064c
Zsolt Lattmann <lattmann@isis.vanderbilt.edu>
2014-07-21 12:48:21 -0500

    Add test tree data generator.

--- client/js/Panels/ObjectBrowser/TreeNavigatorController.js
+++ client/js/Panels/ObjectBrowser/TreeNavigatorController.js
@@ -17,7 +17,7 @@ define([], function (
         self.gmeClient = gmeClient;
 
         // internal data representation for fast access to objects
-  
+
         self.initialize();
     };
 
@@ -31,31 +31,25 @@ define([], function (
         var self = this;
 
         // initialize model structure for view
-        self.$scope.navigator = {
-            items: [],
-            separator: true
+        self.$scope.treeData = {
+            id: 'ROOT',
+            label: 'ROOT',
+            children: []
         };
 
-       
         if (self.gmeClient) {
             self.initWithClient();
         } else {
             self.initTestData();
         }
 
-        // only root is selected by default
-        self.$scope.navigator = {
-            items: [
-                self.root
-            ],
-            separator: true
-        };
+        console.log(self.$scope.treeData);
     };
 
     TreeNavigatorController.prototype.initTestData = function () {
         var self = this;
 
-        //self.dummyTreeDataGenerator('Project', 20);
+        self.dummyTreeDataGenerator(self.$scope.treeData, 'Node item ', 20, 5);
     };
 
     TreeNavigatorController.prototype.initWithClient = function () {
@@ -64,27 +58,45 @@ define([], function (
         // register all event listeners on gmeClient
 
         throw 'Not implemented';
-        
-    };
-      
 
-    TreeNavigatorController.prototype.dummyTreeDataGenerator = function (name, maxCount) {
+    };
+
+    TreeNavigatorController.prototype.addNode = function (treeNode, id) {
+        var self = this,
+            newTreeNode;
+
+        newTreeNode = {
+            id: id,
+            label: id,
+            children: []
+        };
+
+        treeNode.children.push(newTreeNode);
+
+        return newTreeNode;
+    };
+
+    TreeNavigatorController.prototype.dummyTreeDataGenerator = function (treeNode, name, maxCount, levels) {
         var self = this,
             i,
             id,
             count,
-            rights;
+            childNode;
 
-        count = Math.max(Math.round(Math.random() * maxCount), 3);
+        levels = levels || 0;
+
+        count = Math.round(Math.random() * maxCount) + 1;
 
         for (i = 0; i < count; i += 1) {
-            id = name + '_' + i;
-          
+            id = name + i;
 
-            self.addProject(id, rights);
+            childNode = self.addNode(treeNode, id);
+
+            if (levels > 0) {
+                self.dummyTreeDataGenerator(childNode, id + '.', maxCount, levels - 1);
+            }
         }
     };
 
-    
     return TreeNavigatorController;
 });
