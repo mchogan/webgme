5d06ce15acb6c4855815942c934648e50427d21f
Laszlo Juracz <laszlo.juracz@gmail.com>
2014-07-31 15:52:22 -0500

    propertygrid skeleton

---
+++ src/client/ui-components/PropertyGrid/PropertyGrid.js
@@ -0,0 +1,37 @@
+/*globals define, angular, alert*/
+
+
+define([
+    'angular',
+    'text!./templates/PropertyGrid.html',
+    'css!./styles/propertyGrid.css'
+
+], function(
+    ng,
+    template ){
+
+    "use strict";
+
+    angular.module(
+        'isis.ui.propertyGrid',
+        [
+        ]
+
+    ).directive(
+        'propertyGrid',
+         function () {
+
+             return {
+                 scope: {
+                     items: '=',
+                     config: '='
+                 },
+                 restrict: 'E',
+                 replace: true,
+                 template: template
+
+             };
+    });
+
+
+});
