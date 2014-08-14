61cca57088a79efc89766979eba8968faac69cad
Zsolt Lattmann <lattmann@isis.vanderbilt.edu>
2014-08-14 08:39:32 -0500

    PropertyGrid collect attributes/pointers/registry values

--- src/client/js/Panels/PropertyEditor/PropertyGridController.js
+++ src/client/js/Panels/PropertyEditor/PropertyGridController.js
@@ -171,9 +171,7 @@ define([
             pattern[self.currentObjectId] = { "children": 0 };
 
             self.territoryId = self.gmeClient.addUI(self, function (events) {
-                var event,
-                    nodeObj,
-                    i;
+                var event;
 
                 if (events.length === 0) {
                     return;
@@ -182,15 +180,7 @@ define([
                 event = events[0];
 
                 if (event.etype === 'load' || event.etype === 'update') {
-                    nodeObj = self.gmeClient.getNode(event.eid);
-
-
-                    self.$scope.items.push({
-                        id      : 'Name',
-                        label   : 'Name',
-                        value   : nodeObj.getAttribute('name'),
-                        onChange: function () {}
-                    });
+                    self.updateProperties(event.eid);
 
                 } else if (event.etype === 'unload') {
                     self.$scope.items = [];
@@ -207,6 +197,105 @@ define([
 
         } else {
             self.logger.warning('No active object.');
+        }
+    };
+
+    PropertyGridController.prototype.updateProperties = function (nodeId) {
+        var self = this,
+            nodeObj = self.gmeClient.getNode(nodeId),
+            i,
+
+            attributes,
+            attributeNames,
+            registry,
+            registryNames,
+            pointers,
+            pointerNames,
+
+            onChange;
+
+        onChange = function () {
+            self.logger.warning('TODO: handle property change event: ' + JSON.stringify(arguments));
+        };
+
+        // TODO: this is read-only
+        self.$scope.items.push({
+            id      : 'guid',
+            label   : 'GUID',
+            value   : nodeObj.getGuid(),
+            onChange: null
+        });
+
+        // TODO: this should be a link, when we copy the value
+        self.$scope.items.push({
+            id      : 'id',
+            label   : 'ID',
+            value   : nodeObj.getId(),
+            onChange: null
+        });
+
+        // attributes
+        attributes = {
+            id      : 'attributes',
+            label   : 'Attributes',
+            value   : [],
+            onChange: onChange
+        };
+
+        self.$scope.items.push(attributes);
+
+        attributeNames = nodeObj.getAttributeNames();
+
+        for (i = 0; i < attributeNames.length; i += 1) {
+            // TODO: handle types and complex values
+
+            attributes.value.push({
+                id      : attributeNames[i],
+                label   : attributeNames[i],
+                value   : nodeObj.getAttribute(attributeNames[i])
+            });
+        }
+
+        // pointers
+        pointers = {
+            id      : 'pointers',
+            label   : 'Pointers',
+            value   : [],
+            onChange: onChange
+        };
+
+        self.$scope.items.push(pointers);
+
+        pointerNames = nodeObj.getPointerNames();
+
+        for (i = 0; i < pointerNames.length; i += 1) {
+            // TODO: handle types and complex values
+            pointers.value.push({
+                id      : pointerNames[i],
+                label   : pointerNames[i],
+                value   : nodeObj.getPointer(pointerNames[i])
+            });
+        }
+
+        // registry
+        registry = {
+            id      : 'registry',
+            label   : 'Registry',
+            value   : [],
+            onChange: onChange
+        };
+
+        self.$scope.items.push(registry);
+
+        registryNames = nodeObj.getRegistryNames();
+
+        for (i = 0; i < registryNames.length; i += 1) {
+            // TODO: handle types and complex values
+            registry.value.push({
+                id      : registryNames[i],
+                label   : registryNames[i],
+                value   : nodeObj.getRegistry(registryNames[i])
+            });
         }
     };
 
