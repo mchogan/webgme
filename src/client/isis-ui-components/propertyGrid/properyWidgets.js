38687e3b1263959e44130b90d39bfeb9a86ac3e5
Laszlo Juracz <laszlo.juracz@gmail.com>
2014-08-15 14:19:55 -0500

    snapshot

---
+++ src/client/isis-ui-components/propertyGrid/properyWidgets.js
@@ -0,0 +1,17 @@
+/*globals define, angular, alert*/
+
+
+define([
+  'angular'
+
+], function (ng, template) {
+
+  "use strict";
+
+  angular.module(
+    'isis.ui.propertyWidgets',
+    []
+  );
+
+
+});
