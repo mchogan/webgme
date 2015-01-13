0367d7f71330f3ccbd930d08e6e9f7d3f2814ffb
kecso <tkecskes@isis.vanderbilt.edu>
2015-01-13 12:33:30 -0600

    starting to create merge page for webgme...

--- dist/webgme.classes.build.js
+++ dist/webgme.classes.build.js
@@ -24808,11 +24808,15 @@ define('webgme.classes',
   [
     'client',
     'blob/BlobClient',
-    'js/Utils/InterpreterManager'
-  ], function (Client, BlobClient, InterpreterManager) {
+    'js/Utils/InterpreterManager',
+    'core/core',
+    'storage/clientstorage'
+  ], function (Client, BlobClient, InterpreterManager,Core,Storage) {
     GME.classes.Client = Client;
     GME.classes.BlobClient = BlobClient;
     GME.classes.InterpreterManager = InterpreterManager;
+    GME.classes.Core = Core;
+    GME.classes.Storage = Storage;
   });
 
 
--- utils/build/webgme.classes/webgme.classes.js
+++ utils/build/webgme.classes/webgme.classes.js
@@ -2,9 +2,13 @@ define('webgme.classes',
   [
     'client',
     'blob/BlobClient',
-    'js/Utils/InterpreterManager'
-  ], function (Client, BlobClient, InterpreterManager) {
+    'js/Utils/InterpreterManager',
+    'core/core',
+    'storage/clientstorage'
+  ], function (Client, BlobClient, InterpreterManager,Core,Storage) {
     GME.classes.Client = Client;
     GME.classes.BlobClient = BlobClient;
     GME.classes.InterpreterManager = InterpreterManager;
+    GME.classes.Core = Core;
+    GME.classes.Storage = Storage;
   });
